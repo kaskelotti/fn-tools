f39b7b89db0be1bc6fef4578ce726699d60c29b9
kaskelotti <notinuse>
2018-10-05 19:57:22 +0300

    Refactoring: ES6 & Readability
    1. Taking ES6 features into use
    - arrow functions
    - constants
    - using array spread operation for list concat
    
    2. String operations to separated to own inner functions in order to reduce nested ifs
    
    3. If statemets changed to ternaries and other readability improvements where applicable

--- src/fn-tools.js
+++ src/fn-tools.js
@@ -1,15 +1,14 @@
 var fn = (function(fn) {
   'use strict';
 
-  var utils = {
-    createPredicate: function(criteria) {
-      var type = typeof criteria;
+  const utils = {
+    isString: (input) => typeof input === "string",
+    createPredicate: (criteria) => {
+      const type = typeof criteria;
 
       // NOTE: typeof null == 'object'
       if(criteria === null) {
-        return function(value) {
-          return value === null;
-        };
+        return (value) => value === null;
       }
 
       if(type === "function") {
@@ -17,87 +16,91 @@ var fn = (function(fn) {
       }
 
       if(type === "string" || type === "number" || type === "boolean") {
-        return function(value) {
-          return value === criteria;
-        };
+        return (value) => value === criteria;
       }
 
       throw new TypeError("Sorry, only supports basic type, null or function criteria :(");
     }
   };
 
-  fn.head = function(list) {
-    if(!list || list.length === 0) {
-      return null;
-    }
-    return list[0];
-  };
+  fn.head = (list) => !list || list.length === 0 ?
+    null :
+    list[0];
 
-  fn.tail = function(list) {
-    if(!list && !(typeof list === "string")) {
-      return null;
-    }
-
-    if(list.length <= 1) {
-      if(typeof list === "string") {
-        return "";
+  fn.tail = (collection) => {
+    const _stringTail = (str) => {
+      if(str === null) {
+        return null;
       }
-      return [];
-    }
-    return list.slice(1);
-  };
 
-  fn.drop = function(n, list) {
-    if(!list) {
-      if(typeof list === "string") {
-        return "";
-      }
-      return null;
-    }
-
-    if(list.length <= n) {
-      if(typeof list === "string") {
-        return "";
-      }
-      return [];
-    }
-    else {
-      return list.slice(n);
-    }
-  };
-
-  fn.take = function(n, list) {
-    var copy;
-
-    if(!list && !(typeof list === "string")) {
-      return null;
-    }
-
-    if(n >= list.length) {
-      return list;
-    }
-    else {
-      if(typeof list === "string") {
-        return list.substring(0, n);
-      }
-      else {
-          copy = list.slice();
-          copy.splice(n);
-          return copy;
-      }
-    }
-  };
-
-  fn.partial = function(f /* arguments */) {
-    var args = fn.tail(Array.prototype.slice.call(arguments));
-
-    return function() {
-      var _args = Array.prototype.slice.call(arguments);
-      return f.apply(null, args.concat(_args));
+      return str.length <= 1 ?
+        "" :
+        str.slice(1);
     };
+    const _tail = (list) => {
+      if(!list) {
+        return null;
+      }
+
+      return list.length <= 1 ?
+        [] :
+        list.slice(1);
+    };
+
+    return utils.isString(collection) ?
+      _stringTail(collection) :
+      _tail(collection);
   };
 
-  fn.fold = function(f, seed, list) {
+  fn.drop = (n, collection) => {
+    const _stringDrop = (str) => {
+      if(!str) {
+        return "";
+      }
+
+      return str.length < n ?
+        "" :
+        str.slice(n);
+    };
+    const _drop = (list) => {
+      if(!list) {
+        return null;
+      }
+
+      return list.length <= n ? [] : list.slice(n);
+    };
+
+    return utils.isString(collection) ?
+      _stringDrop(collection) :
+      _drop(collection);
+  };
+
+  fn.take = (n, collection) => {
+    const _stringTake = (str) => str.substring(0, n);
+    const _take = (list) => {
+      const _copyAndTake = (list) => {
+        const copy = list.slice();
+        copy.splice(n);
+        return copy;
+      };
+
+      if(!list) {
+        return null;
+      }
+
+      return n >= list.length ?
+        list :
+        _copyAndTake(list);
+    };
+
+    return utils.isString(collection) ?
+      _stringTake(collection) :
+      _take(collection);
+  };
+
+  fn.partial = (f, ...partialArgs) => (...args) => f.apply(null, partialArgs.concat(args));
+
+  fn.fold = (f, seed, list) => {
     if(list.length <= 1) {
       return f(seed, fn.head(list));
     }
@@ -109,37 +112,27 @@ var fn = (function(fn) {
       }
   };
 
-  fn.all = function(predicate, list) {
+  fn.all = (predicate, list) => {
     // fold expects a function with two arguments,
     // thus wrapping the given predicate in order to use the seed
-    var wrapper = function(seed, value) {
-      return seed && predicate(value);
-    };
+    const _wrapper = (seed, value) => seed && predicate(value);
 
-    if(!list || list.length === 0) {
-      return false;
-    }
-    else {
-      return fn.fold(wrapper, true, list);
-    }
+    return !list || list.length === 0 ?
+      false :
+      fn.fold(_wrapper, true, list);
   };
 
-  fn.any = function(predicate, list) {
+  fn.any = (predicate, list) => {
     // fold expects a function with two arguments,
     // thus wrapping the given predicate in order to use the seed
-    var wrapper = function(seed, value) {
-      return seed || predicate(value);
-    };
+    const _wrapper = (seed, value) => seed || predicate(value);
 
-    if(!list || list.length === 0) {
-      return false;
-    }
-    else {
-      return fn.fold(wrapper, false, list);
-    }
+    return !list || list.length === 0 ?
+      false :
+      fn.fold(_wrapper, false, list);
   };
 
-  fn.forEach = function(f, list) {
+  fn.forEach = (f, list) => {
     if(list.length === 0) {
       return;
     }
@@ -153,25 +146,26 @@ var fn = (function(fn) {
     }
   };
 
-  fn.contains = function(criteria, list) {
-    var predicate = utils.createPredicate(criteria);
+  fn.contains = (criteria, list) => {
+    const predicate = utils.createPredicate(criteria);
     return fn.any(predicate, list);
   };
 
-  fn.filter = function(criteria, list) {
-    var predicate = utils.createPredicate(criteria);
-    var _filter = function(list, filtered) {
-      var head = fn.head(list);
+  fn.filter = (criteria, list) => {
+    const predicate = utils.createPredicate(criteria);
+    const _filter = (list, filtered) => {
+      const _performFiltering = (head) => predicate(head) ?
+        [...filtered, head] :
+        filtered;
 
-      if(list.length === 1) {
-        return predicate(head) ? filtered.concat(head) : filtered;
-      }
-      else {
-        return _filter(
-                      fn.tail(list),
-                      predicate(head) ? filtered.concat(head) : filtered);
-      }
-    };
+      const head = fn.head(list);
+
+      return list.length === 1 ?
+        _performFiltering(head) :
+        _filter(
+                fn.tail(list),
+                _performFiltering(head));
+      };
 
     if(!list) {
       return null;
@@ -181,50 +175,37 @@ var fn = (function(fn) {
       return [];
     }
 
-    return _filter(list,[]);
+    return _filter(list, []);
   };
 
-  fn.map = function(f, list) {
-    var _map = function(f, list, mod) {
-      if(list.length === 1) {
-        mod.push(f(fn.head(list)));
-        return mod;
-      }
-      else {
-        mod.push(f(fn.head(list)));
-        return _map(f, fn.tail(list), mod);
-      }
+  fn.map = (f, list) => {
+    const _map = (f, list, mod) => {
+      const mapped = [...mod, f(fn.head(list))];
+
+      return list.length === 1 ?
+        mapped :
+        _map(f, fn.tail(list), mapped);
     };
 
-    if(list.length === 0) {
-      return [];
-    }
-    else {
-      return _map(f, list, []);
-    }
+    return list.length === 0 ?
+      [] :
+      _map(f, list, []);
   };
 
-  fn.pipe = function(value /* functions */) {
-    var _pipe = function(value, fns) {
-      var f = fn.head(fns);
+  fn.pipe = (value, ...fns) => {
+    const _pipe = (value, fns) => {
+      const f = fn.head(fns);
 
-      if(fns.length === 1) {
-        return f(value);
-      }
-      else {
-        return _pipe(
-                    f(value),
-                    fn.tail(fns));
-      }
+      return fns.length === 1 ?
+        f(value) :
+        _pipe(
+              f(value),
+              fn.tail(fns));
     };
 
-    var args = fn.tail(Array.prototype.slice.call(arguments));
-    if(!args || args.length === 0) {
-      return value;
-    }
-    else {
-      return _pipe(value, args);
-    }
+    return !fns || fns.length === 0 ?
+      value :
+      _pipe(value, fns);
   };
 
   return fn;
